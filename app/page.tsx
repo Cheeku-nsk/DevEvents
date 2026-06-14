dc1f6366a51d088cdf17a5c40b266ec55fa80722
Cheeku-nsk <kousikvivek71@gmail.com>
2026-06-14 16:13:52 +0530

    load real events

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import EventCard from "@/components/EventCard"
 import ExploreBtn from "@/components/ExploreBtn"
 import { IEvent } from "@/database";
-import  events  from "@/lib/constants";
+
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Page = async () => {
-  // const response = await fetch(`${BASE_URL}/api/events`)
-  // const { events } = await response.json();
+  const response = await fetch(`${BASE_URL}/api/events`)
+  const { events } = await response.json();
 
   return (
     <section>
@@ -16,7 +16,7 @@ const Page = async () => {
       <div className="mt-20 space-y-4">
         <h3>Featured Events</h3>
           <ul className="events">
-                      {events && events.length > 0 && events.map((event) => (
+                      {events && events.length > 0 && events.map((event: IEvent) => (
                             <li key={event.title} className="list-none">
                               <EventCard {...event} />
                             </li>
